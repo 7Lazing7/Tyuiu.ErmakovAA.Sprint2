faebcf6c9f565bbfd5dbce70f6d7c1ebfb90ab22
Alexsandr Ermokov <Tyuiu.ErmakovA.A@outlook.com>
2024-10-17 13:04:12 +0500

    Task6

--- Tyuiu.ErmakovAA.Sprint2.Task6.V9.Lib/DataService.cs
+++ Tyuiu.ErmakovAA.Sprint2.Task6.V9.Lib/DataService.cs
@@ -35,9 +35,9 @@ namespace Tyuiu.ErmakovAA.Sprint2.Task6.V9.Lib
                         throw new ArgumentException($"День месяца должен быть от 1 до 12.Значение {m}");
                 }
 
-                if (m < 1 || n > 12 || m < 1 || n > DateTime.DaysInMonth(2024, m))
+                if (n < 1 || n > daysInMonth)
                 {
-                    throw new ArgumentOutOfRangeException("Неверные значения месяца или дня");
+                    throw new ArgumentOutOfRangeException("Некорректный номер дня.");
                 }
                 if (n < daysInMonth)
                 {
@@ -52,63 +52,11 @@ namespace Tyuiu.ErmakovAA.Sprint2.Task6.V9.Lib
                 {
                     return ($"{m + 1}/1");
                 }
-                if (m < 1 || n > 12 || m < 1 || n > DateTime.DaysInMonth(2024, m))
-                {
-                    throw new ArgumentOutOfRangeException("Неверные значения месяца или дня");
-                }
-
                 DateTime date = new DateTime(2024, m, n);
                 DateTime nextDay = date.AddDays(1);
 
-                
+                // Форматируем дату как MM.dd
                 return nextDay.ToString("MM.dd");
-            }
-        }
-    }
-}          /*
-
-                switch (m)
-                {
-                    case 1: return "1";
-                    case 3: return "3";
-                    case 5: return "5";
-                    case 7: return "7";
-                    case 8: return "8 ";
-                    case 10: return "10 ";
-                    case 12:return "12 ";
-                        daysInMonth = 31; break;
-
-                    case 4: return "4";
-                    case 6: return "6";
-                    case 9: return "9 ";
-                    case 11:return "11 ";
-                        daysInMonth = 30; break;
-
-                    case 2:
-                        return "Февраль";
-                        daysInMonth = 28; break;
-
-                    default:
-                        throw new ArgumentException($"День месяца должен быть от 1 до 12.Значение {m}");
-                }
-
-                if (n < 1 || n > daysInMonth)
-                {
-                    throw new ArgumentOutOfRangeException("Некорректный номер дня.");
-                }
-                if (n < daysInMonth)
-                {
-                    return  ($"{m}/{n + 1}");
-                }
-                else if (n == 12)
-                {
-
-                    return ($"1/1");
-                }
-                else
-                {
-                    return ($"{m + 1}/1");
-                }
             }
         }
     }
