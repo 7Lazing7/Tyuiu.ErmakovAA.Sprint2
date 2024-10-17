01446c1dee12960f7ecd2b01e9d8be9291ce320b
Alexsandr Ermokov <Tyuiu.ErmakovA.A@outlook.com>
2024-10-17 13:06:00 +0500

    Task6

--- Tyuiu.ErmakovAA.Sprint2.Task6.V9.Lib/DataService.cs
+++ Tyuiu.ErmakovAA.Sprint2.Task6.V9.Lib/DataService.cs
@@ -14,8 +14,8 @@ namespace Tyuiu.ErmakovAA.Sprint2.Task6.V9.Lib
                     case 3: return "3";
                     case 5: return "5";
                     case 7: return "7";
-                    case 8: return "10.08 ";
-                    case 10: return "10 ";
+                    case 8: return "8";
+                    case 10: return "10";
                     case 12:
                         return "12 ";
                         daysInMonth = 31; break;
@@ -23,12 +23,10 @@ namespace Tyuiu.ErmakovAA.Sprint2.Task6.V9.Lib
                     case 4: return "4";
                     case 6: return "6";
                     case 9: return "9 ";
-                    case 11:
-                        return "11 ";
+                    case 11:return "11 ";
                         daysInMonth = 30; break;
 
-                    case 2:
-                        return "Февраль";
+                    case 2:return "Февраль";
                         daysInMonth = 28; break;
 
                     default:
