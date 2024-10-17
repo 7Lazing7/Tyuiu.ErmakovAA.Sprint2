7b48e37471eb3b8a545bbb8fc6696ca1538cdb39
Alexsandr Ermokov <Tyuiu.ErmakovA.A@outlook.com>
2024-10-17 12:58:33 +0500

    Task6

--- Tyuiu.ErmakovAA.Sprint2.Task6.V9.Lib/DataService.cs
+++ Tyuiu.ErmakovAA.Sprint2.Task6.V9.Lib/DataService.cs
@@ -5,6 +5,37 @@ namespace Tyuiu.ErmakovAA.Sprint2.Task6.V9.Lib
     {
         public string FindDateOfNextDay(int m, int n)
         {
+            {
+
+                int daysInMonth;
+                switch (m)
+                {
+                    case 1: return "1";
+                    case 3: return "3";
+                    case 5: return "5";
+                    case 7: return "7";
+                    case 8: return "8 ";
+                    case 10: return "10 ";
+                    case 12:
+                        return "12 ";
+                        daysInMonth = 31; break;
+
+                    case 4: return "4";
+                    case 6: return "6";
+                    case 9: return "9 ";
+                    case 11:
+                        return "11 ";
+                        daysInMonth = 30; break;
+
+                    case 2:
+                        return "Февраль";
+                        daysInMonth = 28; break;
+
+                    default:
+                        throw new ArgumentException($"День месяца должен быть от 1 до 12.Значение {m}");
+                }
+
+
                 if (m < 1 || n > 12 || m < 1 || n > DateTime.DaysInMonth(2024, m))
                 {
                     throw new ArgumentOutOfRangeException("Неверные значения месяца или дня");
@@ -15,11 +46,10 @@ namespace Tyuiu.ErmakovAA.Sprint2.Task6.V9.Lib
 
                 
                 return nextDay.ToString("MM.dd");
-           
+            }
         }
     }
 }
-
                 /*
 
                 switch (m)
